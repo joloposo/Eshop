1dfdafb087b7f2dba4574d6fa07a0312fd6bd30c
Michal Vavrek <michal.vavrek@coratec.sk>
2025-04-23 23:31:10 +0200

    web api

---
+++ Eshop.Domain/Product.cs
@@ -0,0 +1,37 @@
+﻿namespace Eshop.Domain
+{
+    public class Product
+    {
+        public int Id { get; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+
+        public Product(int id, string name, string description, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+
+        public void Update(string title, string description, decimal price)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+            {
+                throw new ArgumentException("Title cannot be empty", nameof(title));
+            }
+            if (string.IsNullOrWhiteSpace(description))
+            {
+                throw new ArgumentException("Description cannot be empty", nameof(description));
+            }
+            if (price <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(price), "Price must be greater than zero");
+            }
+            Name = title;
+            Description = description;
+            Price = price;
+        }
+    }
+}
